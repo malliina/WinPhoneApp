63c76c522219593cb5a9b17781b0bc17f542e7a4
malliina <malliina123@gmail.com>
2015-01-25 21:16:34 +0200

    vm

---
+++ WinPhoneApp/ViewModels/TestVM.cs
@@ -0,0 +1,12 @@
+﻿using Windows.Security.ExchangeActiveSyncProvisioning;
+
+namespace WinPhoneApp.ViewModels {
+    public class TestVM : ViewModelBase {
+        private string deviceName;
+        public string DeviceName { get { return deviceName; } }
+        public TestVM() {
+            var deviceInfo = new EasClientDeviceInformation();
+            deviceName = deviceInfo.FriendlyName;
+        }
+    }
+}
